340b85f74784df798265ff63721a122ed6eac09a
İHLASMERT <ihlas1mert@gmail.com>
2022-11-19 21:38:30 +0300

    added randomly generate

--- js.js
+++ js.js
@@ -3,11 +3,30 @@ const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
 const possibleChoice = document.querySelectorAll("button");
 
-
+let userChoice;
+let computerChoice;
 
 possibleChoice.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
   })
 );
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNumber);
+
+
+if (randomNumber === 1) {
+  computerChoice = "rock";
+}
+if (randomNumber === 2) {
+  computerChoice = "scissors";
+}
+if (randomNumber === 3) {
+  computerChoice = "paper";
+}
+computerChoiceDisplay.innerHTML = computerChoice
+}
